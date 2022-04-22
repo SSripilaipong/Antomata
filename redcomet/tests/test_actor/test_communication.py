6bedbbae328ed1f0918c6f0e8c61b6fcf078de59
SSripilaipong <SHSnail@mail.com>
2022-04-22 18:56:55 +0700

    test(redcomet): add completed unit test test_should_tell_message_to_another_actor()
    Refs: #3

---
+++ redcomet/tests/test_actor/test_communication.py
@@ -0,0 +1,66 @@
+from redcomet.actor.abstract import ActorAbstract
+from redcomet.actor.ref import ActorRef
+from redcomet.message.abstract import MessageAbstract
+from redcomet.queue.abstract import QueueAbstract
+from redcomet.queue.default import DefaultQueue
+from redcomet.system import ActorSystem
+
+
+class MyStringMessage(MessageAbstract):
+    def __init__(self, value: str):
+        self.value = value
+
+
+class IntroduceMessage(MessageAbstract):
+    def __init__(self, ref: ActorRef):
+        self.ref = ref
+
+
+class First(ActorAbstract):
+    def __init__(self, recv_queue: QueueAbstract):
+        self._recv_queue = recv_queue
+        self._second = None
+
+    def receive(self, message: MessageAbstract):
+        if isinstance(message, IntroduceMessage):
+            self._second = message.ref
+        elif isinstance(message, MyStringMessage):
+            if message.value == "hi to second":
+                self._second.tell(MyStringMessage("HI FROM FIRST"))
+            else:
+                self._recv_queue.put(message.value)
+        else:
+            raise NotImplementedError()
+
+
+class Second(ActorAbstract):
+    def __init__(self, first: ActorRef, recv_queue: QueueAbstract):
+        self._first = first
+        self._recv_queue = recv_queue
+
+    def receive(self, message: MessageAbstract):
+        if isinstance(message, MyStringMessage):
+            if message.value == "hi to first":
+                self._first.tell(MyStringMessage("HI FROM SECOND"))
+            else:
+                self._recv_queue.put(message.value)
+        else:
+            raise NotImplementedError()
+
+
+def test_should_communicate_between_actors():
+    first_queue = DefaultQueue()
+    second_queue = DefaultQueue()
+    with ActorSystem() as system:
+        first = system.spawn(First(first_queue))
+        second = system.spawn(Second(first, second_queue))
+
+        first.tell(IntroduceMessage(second))
+
+        first.tell(MyStringMessage("hi to second"))
+        message_second = second_queue.get(timeout=2)
+        assert message_second == "HI FROM FIRST"
+
+        second.tell(MyStringMessage("hi to first"))
+        message_first = first_queue.get(timeout=2)
+        assert message_first == "HI FROM SECOND"
