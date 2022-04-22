e7c97c6c7b74c4384aaef0e69dac970688ec8494
SSripilaipong <SHSnail@mail.com>
2022-04-22 18:05:31 +0700

    refactor(redcomet): move MyActor in test to global
    Refs: #1

--- redcomet/tests/system/test_instantiate.py
+++ redcomet/tests/system/test_instantiate.py
@@ -10,14 +10,15 @@ class MyStringMessage(MessageAbstract):
         self.value = value
 
 
+class MyActor(ActorAbstract):
+    def __init__(self, recv_queue: QueueAbstract):
+        self._recv_queue = recv_queue
+
+    def receive(self, message: MessageAbstract):
+        self._recv_queue.put(message)
+
+
 def test_should_tell_message():
-    class MyActor(ActorAbstract):
-        def __init__(self, recv_queue: QueueAbstract):
-            self._recv_queue = recv_queue
-
-        def receive(self, message: MessageAbstract):
-            self._recv_queue.put(message)
-
     queue = DefaultQueue()
     system = ActorSystem()
     ref = system.spawn(MyActor(queue))
