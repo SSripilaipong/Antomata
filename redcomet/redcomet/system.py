a75e7036bfafbbda5dc7d067cb26ef4ecbed3aba
SSripilaipong <SHSnail@mail.com>
2022-04-24 17:08:30 +0700

    refactor(redcomet): refactor ActorSystem for more cleaner contructor
    Refs: #7

--- redcomet/redcomet/system.py
+++ redcomet/redcomet/system.py
@@ -1,7 +1,7 @@
+from typing import List, Tuple
+
 from redcomet.actor.ref import ActorRef
-from redcomet.base.actor import ActorRefAbstract
 from redcomet.base.actor.abstract import ActorAbstract
-from redcomet.base.cluster.abstract import ClusterAbstract
 from redcomet.base.message.abstract import MessageAbstract
 from redcomet.cluster import Cluster
 from redcomet.executor import Executor
@@ -16,25 +16,13 @@ class ActorSystem:
     def __init__(self):
         self._incoming_messages = DefaultQueue()
 
-        inbox = Inbox("main")
-        outbox = Outbox("main")
-
-        executor0 = Executor("node0")
-        inbox0 = Inbox("node0")
-        outbox0 = Outbox("node0")
-
-        outbox.register_inbox(inbox, "main")
-        outbox.register_inbox(inbox0, "node0")
-        outbox0.register_inbox(inbox, "main")
-        outbox0.register_inbox(inbox0, "node0")
-
-        self._gateway = GatewayNode.create("main", outbox, inbox, self._incoming_messages)
-        self._node = Node.create("node0", executor0, outbox0, inbox0)
-
         self._cluster = Cluster()
 
-        executor0.set_cluster(self._cluster)
+        self._gateway, gateway_inbox, gateway_outbox = _create_gateway_node("main", self._incoming_messages)
+        self._node, inbox0, outbox0 = _create_worker_node("node0", self._cluster)
         self._cluster.set_node(self._node)
+
+        _wire_outboxes_to_inboxes([("main", gateway_inbox), ("node0", inbox0)], [gateway_outbox, outbox0])
 
     def spawn(self, actor: ActorAbstract) -> ActorRef:
         return self._cluster.spawn(actor, self._gateway, "main")
@@ -47,3 +35,26 @@ class ActorSystem:
 
     def fetch_message(self, timeout: float = None) -> MessageAbstract:
         return self._incoming_messages.get(timeout=timeout)
+
+
+def _create_gateway_node(node_id: str, incoming_messages: DefaultQueue) -> (GatewayNode, Inbox, Outbox):
+    inbox = Inbox(node_id)
+    outbox = Outbox(node_id)
+
+    return GatewayNode.create(node_id, outbox, inbox, incoming_messages), inbox, outbox
+
+
+def _create_worker_node(node_id: str, cluster: Cluster) -> (Node, Inbox, Outbox):
+    executor = Executor(node_id)
+    inbox = Inbox(node_id)
+    outbox = Outbox(node_id)
+
+    executor.set_cluster(cluster)
+
+    return Node.create(node_id, executor, outbox, inbox), inbox, outbox
+
+
+def _wire_outboxes_to_inboxes(inboxes: List[Tuple[str, Inbox]], outboxes: List[Outbox]):
+    for outbox in outboxes:
+        for name, inbox in inboxes:
+            outbox.register_inbox(inbox, name)
