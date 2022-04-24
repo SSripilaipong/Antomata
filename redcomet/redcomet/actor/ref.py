880654634aa9b5186f05ce3d565aebf216d9f9a5
SSripilaipong <SHSnail@mail.com>
2022-04-24 18:32:25 +0700

    refactor(redcomet): rename parameters of ActorRef to local_issuer_id and ref_id
    Refs: #9

--- redcomet/redcomet/actor/ref.py
+++ redcomet/redcomet/actor/ref.py
@@ -4,16 +4,16 @@ from redcomet.base.outbox import OutboxAbstract
 
 
 class ActorRef(ActorRefAbstract):
-    def __init__(self, outbox: OutboxAbstract, local_id: str, receiver_id: str):
+    def __init__(self, outbox: OutboxAbstract, local_issuer_id: str, ref_id: str):
         self._outbox = outbox
-        self._local_id = local_id
-        self._receiver_id = receiver_id
+        self._local_issuer_id = local_issuer_id
+        self._ref_id = ref_id
 
     def bind(self, ref: 'ActorRef') -> 'ActorRef':
-        return ActorRef(ref._outbox, ref._local_id, self._receiver_id)
+        return ActorRef(ref._outbox, ref._local_issuer_id, self._ref_id)
 
     def tell(self, message: MessageAbstract):
-        self._outbox.send(message, self._local_id, self._receiver_id)
+        self._outbox.send(message, self._local_issuer_id, self._ref_id)
 
     def __repr__(self) -> str:
-        return f"ActorRef(..., local_id={self._local_id!r}, receiver_id={self._receiver_id!r})"
+        return f"ActorRef(..., local_issuer_id={self._local_issuer_id!r}, ref_id={self._ref_id!r})"
