e67358bad0854dce8d9613027c01c56a08fd3899
SSripilaipong <SHSnail@mail.com>
2022-04-25 18:07:28 +0700

    refactor(redcomet): rename actor_id -> target in Address
    Refs: #14

--- redcomet/redcomet/base/messaging/address.py
+++ redcomet/redcomet/base/messaging/address.py
@@ -2,25 +2,25 @@ from typing import Optional
 
 
 class Address:
-    def __init__(self, node_id: Optional[str], actor_id: str):
+    def __init__(self, node_id: Optional[str], target: str):
         self._node_id = node_id
-        self._actor_id = actor_id
+        self._target = target
 
     @classmethod
-    def on_local(cls, actor_id: str) -> 'Address':
-        return cls(None, actor_id)
+    def on_local(cls, target: str) -> 'Address':
+        return cls(None, target)
 
     @property
     def node_id(self) -> str:
         return self._node_id
 
     @property
-    def actor_id(self) -> str:
-        return self._actor_id
+    def target(self) -> str:
+        return self._target
 
     def set_node_id(self, node_id: str):
         self._node_id = node_id
 
     def to_str(self) -> str:
         assert self._node_id is not None
-        return f"{self._node_id}.{self.actor_id}"
+        return f"{self._node_id}.{self._target}"
