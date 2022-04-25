1445c6617d50a8fe1210cfb8b02f1703053cf327
SSripilaipong <SHSnail@mail.com>
2022-04-25 22:36:56 +0700

    refactor(redcomet): make GatewayNode inherit from Node
    Refs: #16

--- redcomet/redcomet/node/gateway.py
+++ redcomet/redcomet/node/gateway.py
@@ -1,41 +1,28 @@
-from redcomet.actor.ref import ActorRef
 from redcomet.base.actor import ActorAbstract, ActorRefAbstract
 from redcomet.base.actor.executor import ActorExecutorAbstract
 from redcomet.base.actor.message import MessageAbstract
 from redcomet.base.actor.discovery import ActorDiscovery
 from redcomet.base.messaging.inbox import Inbox
 from redcomet.base.messaging.outbox import Outbox
-from redcomet.base.node import NodeAbstract
 from redcomet.messaging.handler import PacketHandler
+from redcomet.node.synchronous import Node
 from redcomet.queue.abstract import QueueAbstract
 
 
-class GatewayNode(NodeAbstract):
+class GatewayNode(Node):
     def __init__(self, node_id: str, executor: ActorExecutorAbstract, outbox: Outbox,
                  incoming_queue: QueueAbstract, discovery: ActorDiscovery):
-        self._node_id = node_id
-        self._executor = executor
-        self._outbox = outbox
+        super().__init__(node_id=node_id, executor=executor, outbox=outbox, discovery=discovery)
+
         self._incoming_queue = incoming_queue
-        self._discovery = discovery
 
     @classmethod
-    def create(cls, node_id: str, outbox: Outbox, inbox: Inbox, incoming_queue: QueueAbstract, discovery: ActorDiscovery) \
-            -> 'GatewayNode':
+    def create(cls, node_id: str, outbox: Outbox, inbox: Inbox, incoming_queue: QueueAbstract,
+               discovery: ActorDiscovery) -> 'GatewayNode':
         executor = EnqueueExecutor(incoming_queue)
         node = cls(node_id, executor, outbox, incoming_queue, discovery)
         inbox.set_handler(PacketHandler(executor))
         return node
-
-    def issue_actor_ref(self, local_issuer_id: str, ref_id: str) -> ActorRefAbstract:
-        return ActorRef(self._outbox, local_issuer_id, ref_id)
-
-    @property
-    def node_id(self) -> str:
-        return self._node_id
-
-    def register(self, actor: ActorAbstract, actor_id: str) -> str:
-        raise NotImplementedError()
 
 
 class EnqueueExecutor(ActorExecutorAbstract):
