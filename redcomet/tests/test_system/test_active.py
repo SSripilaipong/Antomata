556ad652e2452924169f2ffe93cae799455fbb51
SSripilaipong <SHSnail@mail.com>
2022-04-29 17:02:56 +0700

    refactor(redcomet): add n_worker_nodes paramter in ActorSystem
    Refs: #5

--- redcomet/tests/test_system/test_active.py
+++ redcomet/tests/test_system/test_active.py
@@ -3,6 +3,6 @@ from redcomet.system import ActorSystem
 
 
 def test_should_return_active_node():
-    with ActorSystem.create() as system:
+    with ActorSystem.create(n_worker_nodes=1) as system:
         active_nodes = system.get_active_nodes(timeout=0.5)
         assert len(active_nodes) == 1 and isinstance(active_nodes[0], NodeRef)
