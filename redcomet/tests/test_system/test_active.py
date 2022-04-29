f8c4eb51f1d3c2886a804711c768782d34eeee81
SSripilaipong <SHSnail@mail.com>
2022-04-29 17:21:20 +0700

    test(redcomet): add unit test test_should_return_multiple_active_node_ids()
    Refs: #5

--- redcomet/tests/test_system/test_active.py
+++ redcomet/tests/test_system/test_active.py
@@ -12,3 +12,11 @@ def test_should_return_active_node_id():
     with ActorSystem.create(n_worker_nodes=1, node_id_prefix="local") as system:
         active_nodes = system.get_active_nodes(timeout=0.5)
         assert len(active_nodes) == 1 and active_nodes[0].node_id == "local0"
+
+
+def test_should_return_multiple_active_node_ids():
+    with ActorSystem.create(n_worker_nodes=2, node_id_prefix="local") as system:
+        active_nodes = system.get_active_nodes(timeout=0.5)
+        assert len(active_nodes) == 2
+        assert active_nodes[0].node_id == "local0"
+        assert active_nodes[1].node_id == "local1"
