83ef95bd4257132afa353c2ae80a9e09014c24f0
SSripilaipong <SHSnail@mail.com>
2022-04-29 18:23:13 +0700

    feat(redcomet): add direct_message package in base.messenger package
    Refs: #19

---
+++ redcomet/redcomet/base/messenger/direct_message/box.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+from redcomet.base.actor.message import MessageAbstract
+
+
+class DirectMessageBoxAbstract(ABC):
+    @abstractmethod
+    def get(self, timeout: float) -> MessageAbstract:
+        pass
+
+    @abstractmethod
+    def put(self, item: MessageAbstract):
+        pass
+
+    @property
+    @abstractmethod
+    def ref_id(self) -> str:
+        pass
---
+++ redcomet/redcomet/base/messenger/direct_message/manager.py
@@ -0,0 +1,33 @@
+from abc import abstractmethod, ABC
+
+from redcomet.base.messenger.direct_message.box import DirectMessageBoxAbstract
+
+
+class DirectMessageManagerAbstract(ABC):
+
+    @abstractmethod
+    def create_message_box(self) -> 'DirectMessageBoxRef':
+        pass
+
+    @abstractmethod
+    def destroy_message_box(self, ref_id: str):
+        pass
+
+
+class DirectMessageBoxRef:
+    def __init__(self, box: DirectMessageBoxAbstract, manager: DirectMessageManagerAbstract):
+        self._box = box
+        self._manager = manager
+
+    def get(self, timeout: float):
+        return self._box.get(timeout=timeout)
+
+    @property
+    def ref_id(self) -> str:
+        return self._box.ref_id
+
+    def __enter__(self) -> 'DirectMessageBoxRef':
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._manager.destroy_message_box(self._box.ref_id)
