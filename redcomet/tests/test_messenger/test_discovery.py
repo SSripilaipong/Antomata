efb692319a6c8715fdc0ac730135e49221557ef3
SSripilaipong <SHSnail@mail.com>
2022-05-02 14:13:44 +0700

    test: add completed messenger test test_should_forward_message_to_queried_address()
    Refs: #21

--- redcomet/tests/test_messenger/test_discovery.py
+++ redcomet/tests/test_messenger/test_discovery.py
@@ -5,6 +5,7 @@ from redcomet.base.discovery.ref import ActorDiscoveryRefAbstract
 from redcomet.base.messaging.address import Address
 from redcomet.base.messaging.handler import PacketHandlerAbstract
 from redcomet.base.messaging.packet import Packet
+from redcomet.discovery.message.query.response import QueryAddressResponse
 from redcomet.messenger.factory import create_messenger
 from redcomet.messenger.request import MessageForwardRequest
 
@@ -22,8 +23,13 @@ class MockPacketHandler(PacketHandlerAbstract):
         self.received_packet = packet
 
 
+class DummyQueryAddressResponse(MessageAbstract):
+    pass
+
+
 class MockActorDiscoveryRef(ActorDiscoveryRefAbstract):
-    def __init__(self):
+    def __init__(self, query_response_params=None):
+        self._query_response_params = query_response_params
         self.queried_address = None
 
     def register_address(self, target: str, node_id: str):
@@ -33,7 +39,10 @@ class MockActorDiscoveryRef(ActorDiscoveryRefAbstract):
         self.queried_address = target, requester_node_id, requester_target
 
     def call_on_query_address_response(self, message: MessageAbstract, func: Callable[[str, Address], Any]) -> bool:
-        pass
+        if self._query_response_params:
+            func(*self._query_response_params)
+            return True
+        return False
 
 
 def _create_messenger_with_node_id(handler: PacketHandlerAbstract, node_id: str):
@@ -58,3 +67,16 @@ def test_should_send_query_message_to_discovery_when_address_is_unknown():
     me.bind_discovery(discovery)
     me.receive(MessageForwardRequest(DummyMessage(123), "mine", "yours"), ..., ..., ...)
     assert discovery.queried_address == ("yours", "me", "messenger")
+
+
+def test_should_forward_message_to_queried_address():
+    your_handler = MockPacketHandler()
+    me = _create_messenger_with_node_id(..., "me")
+    you = _create_messenger_with_node_id(your_handler, "you")
+    me.make_connection_to(you)
+    me.bind_discovery(MockActorDiscoveryRef(query_response_params=("yours", Address("you", "yours"))))
+
+    me.receive(MessageForwardRequest(DummyMessage(123), "mine", "yours"), ..., ..., ...)
+    me.receive(DummyQueryAddressResponse(), ..., ..., ...)
+
+    assert your_handler.received_packet.content.value == 123
