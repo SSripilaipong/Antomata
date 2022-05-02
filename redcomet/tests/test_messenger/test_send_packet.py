8be633c28173b68a08dfa5c4bbad4b4b1a18a8f9
SSripilaipong <SHSnail@mail.com>
2022-05-02 12:47:36 +0700

    test: add completed messenger test test_should_receive_packet()
    Refs: 21

--- redcomet/tests/test_messenger/test_send_packet.py
+++ redcomet/tests/test_messenger/test_send_packet.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from redcomet.base.messaging.address import Address
 from redcomet.base.messaging.content import PacketContentAbstract
 from redcomet.base.messaging.handler import PacketHandlerAbstract
@@ -6,13 +8,16 @@ from redcomet.messenger.factory import create_messenger
 
 
 class DummyPacketContent(PacketContentAbstract):
-    pass
+    def __init__(self, value=None):
+        self.value = value
 
 
 class MockPacketHandler(PacketHandlerAbstract):
+    def __init__(self):
+        self.received_packet: Optional[Packet] = None
 
     def handle(self, packet: Packet):
-        pass
+        self.received_packet = packet
 
 
 def test_should_send_packet():
@@ -21,3 +26,14 @@ def test_should_send_packet():
     sender.make_connection_to(receiver)
 
     sender.send_packet(Packet(DummyPacketContent(), Address.on_local("me"), Address("receiver-node", "you")))
+
+
+def test_should_receive_packet():
+    receiver_handler = MockPacketHandler()
+    sender = create_messenger(MockPacketHandler(), node_id="sender-node")
+    receiver = create_messenger(receiver_handler, node_id="receiver-node")
+    sender.make_connection_to(receiver)
+
+    sender.send_packet(Packet(DummyPacketContent(123), Address.on_local("me"), Address("receiver-node", "you")))
+    content: DummyPacketContent = receiver_handler.received_packet.content
+    assert content.value == 123
