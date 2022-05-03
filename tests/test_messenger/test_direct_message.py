51647a526cd61c0197cda67b75e7700cdb8d3ff5
SSripilaipong <SHSnail@mail.com>
2022-05-03 22:57:20 +0700

    test: modify direct_message tests to support parallel
    Refs: #28

--- tests/test_messenger/test_direct_message.py
+++ tests/test_messenger/test_direct_message.py
@@ -14,8 +14,12 @@ class DummyMessage(MessageAbstract):
         return self._ref_id
 
 
+def _create_messenger():
+    return create_messenger(..., parallel=True)
+
+
 def test_should_get_value_from_direct_message_box():
-    messenger = create_messenger(...)
+    messenger = _create_messenger()
     with messenger.create_direct_message_box() as box:
         box.put(DummyMessage("Hello"))
         message: DummyMessage = box.get(timeout=0.1)
@@ -23,7 +27,7 @@ def test_should_get_value_from_direct_message_box():
 
 
 def test_should_put_message_with_ref_id_to_direct_message_box():
-    messenger = create_messenger(...)
+    messenger = _create_messenger()
     with messenger.create_direct_message_box() as box:
         messenger.receive(DummyMessage("Hello", ref_id=box.ref_id), ..., ..., ...)
         message: DummyMessage = box.get(timeout=0.1)
@@ -31,12 +35,12 @@ def test_should_put_message_with_ref_id_to_direct_message_box():
 
 
 def test_should_ignore_message_when_ref_id_is_invalid():
-    messenger = create_messenger(...)
+    messenger = _create_messenger()
     messenger.receive(DummyMessage("Hello", ref_id="abc"), ..., ..., ...)
 
 
 def test_should_ignore_operations_after_box_is_closed():
-    messenger = create_messenger(...)
+    messenger = _create_messenger()
     with messenger.create_direct_message_box() as box:
         pass
     messenger.receive(DummyMessage("Hello", ref_id=box.ref_id), ..., ..., ...)
@@ -44,7 +48,7 @@ def test_should_ignore_operations_after_box_is_closed():
 
 
 def test_should_raise_timeout_error():
-    messenger = create_messenger(...)
+    messenger = _create_messenger()
     with messenger.create_direct_message_box() as box:
         with raises(TimeoutError):
             box.get(timeout=0.0001)
