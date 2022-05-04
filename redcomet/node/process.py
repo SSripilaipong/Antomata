1a72509a6f3def6958aca8b56735039b9c5f801b
SSripilaipong <SHSnail@mail.com>
2022-05-04 13:05:48 +0700

    feat: provide node_id for node test test_should_provide_node_id()
    Refs: #29

--- redcomet/node/process.py
+++ redcomet/node/process.py
@@ -17,6 +17,7 @@ class ProcessNode(NodeAbstract):
 
         self._manager: Optional[NodeManagerAbstract] = None
         self._actor_id: Optional[str] = None
+        self._node_id: Optional[str] = None
 
     def bind_discovery(self, address: Address):
         ref = ActorDiscoveryRef(self._messenger, address, self._actor_id)
@@ -36,11 +37,12 @@ class ProcessNode(NodeAbstract):
         pass
 
     def assign_node_id(self, node_id: str):
+        self._node_id = node_id
         self._actor_id = node_id
 
     @property
     def node_id(self) -> str:
-        pass
+        return self._node_id
 
     @property
     def messenger(self) -> MessengerAbstract:
