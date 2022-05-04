7ff9a8eead0421531bb4ab8c49783bc5f740bc21
SSripilaipong <SHSnail@mail.com>
2022-05-04 12:24:42 +0700

    feat: add parallel flag in create_node()
    Refs: #29

--- redcomet/node/factory.py
+++ redcomet/node/factory.py
@@ -1,14 +1,20 @@
 from redcomet.actor.executor import ActorExecutor
+from redcomet.base.node.abstract import NodeAbstract
 from redcomet.messaging.handler import PacketHandler
 from redcomet.messenger.factory import create_messenger
 from redcomet.node.manager import NodeManager
+from redcomet.node.process import ProcessNode
 from redcomet.node.synchronous import SynchronousNode
 
 
-def create_node() -> SynchronousNode:
+def create_node(parallel=False) -> NodeAbstract:
     executor = ActorExecutor()
-    messenger = create_messenger(PacketHandler(executor), actor_id="messenger")
-    node = SynchronousNode(executor, messenger)
+    if not parallel:
+        messenger = create_messenger(PacketHandler(executor), actor_id="messenger")
+        node = SynchronousNode(executor, messenger)
+    else:
+        messenger = create_messenger(PacketHandler(executor), actor_id="messenger", parallel=True)
+        node = ProcessNode()
     executor.set_node(node)
 
     manager = NodeManager("manager", node)
