088ebbe4139700f7a2e54d09c4d485134178c484
SSripilaipong <SHSnail@mail.com>
2022-05-04 12:57:15 +0700

    test: add node test test_should_bind_manager_to_discovery_ref()
    Refs: #29

--- tests/test_node/test_node.py
+++ tests/test_node/test_node.py
@@ -6,6 +6,7 @@ from redcomet.base.messenger.abstract import MessengerAbstract
 from redcomet.base.messenger.direct_message.ref import DirectMessageBoxRefAbstract
 from redcomet.base.node.abstract import NodeAbstract
 from redcomet.discovery.ref import ActorDiscoveryRef
+from redcomet.node.manager.abstract import NodeManagerAbstract
 from redcomet.node.process import ProcessNode
 
 
@@ -36,6 +37,14 @@ class MockMessenger(MessengerAbstract):
         pass
 
 
+class MockManager(NodeManagerAbstract):
+    def __init__(self):
+        self.bound_discovery = None
+
+    def bind_discovery(self, discovery: ActorDiscoveryRefAbstract):
+        self.bound_discovery = discovery
+
+
 def _create_node(messenger: MessengerAbstract) -> NodeAbstract:
     return ProcessNode(messenger)
 
@@ -46,3 +55,13 @@ def test_should_bind_messenger_to_discovery_ref():
     node.assign_node_id("node")
     node.bind_discovery(Address("my", "discovery"))
     assert messenger.bound_discovery == ActorDiscoveryRef(messenger, Address("my", "discovery"), "node")
+
+
+def test_should_bind_manager_to_discovery_ref():
+    manager = MockManager()
+    messenger = MockMessenger()
+    node = _create_node(messenger)
+    node.assign_node_id("node")
+    node.assign_manager(manager)
+    node.bind_discovery(Address("my", "discovery"))
+    assert manager.bound_discovery == ActorDiscoveryRef(messenger, Address("my", "discovery"), "node")
