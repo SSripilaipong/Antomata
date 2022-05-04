74315ed057661d3a68d4852e622d3eff9604e90a
SSripilaipong <SHSnail@mail.com>
2022-05-04 13:39:32 +0700

    feat: add issue_*_ref() in ProcessNode
    Refs: #29

--- redcomet/node/process.py
+++ redcomet/node/process.py
@@ -1,11 +1,13 @@
 from typing import Optional
 
+from redcomet.actor.ref import ActorRef
 from redcomet.base.actor import ActorRefAbstract
 from redcomet.base.actor.abstract import ActorAbstract
 from redcomet.base.cluster.ref import ClusterRefAbstract
 from redcomet.base.messaging.address import Address
 from redcomet.base.messenger.abstract import MessengerAbstract
 from redcomet.base.node.abstract import NodeAbstract
+from redcomet.cluster.ref import ClusterRef
 from redcomet.discovery.ref import ActorDiscoveryRef
 from redcomet.node.executor import ActorExecutorAbstract
 from redcomet.node.manager.abstract import NodeManagerAbstract
@@ -26,14 +28,14 @@ class ProcessNode(NodeAbstract):
         self._messenger.bind_discovery(ref)
         self._manager.bind_discovery(ref)
 
-    def issue_actor_ref(self, local_issuer_id: str, address: Address) -> ActorRefAbstract:
-        pass
-
     def issue_cluster_ref(self, local_issuer_id: str) -> ClusterRefAbstract:
-        pass
+        return ClusterRef(self._messenger, local_issuer_id, "main", "cluster")
+
+    def issue_actor_ref(self, local_issuer_id: str, address: Address) -> ActorRefAbstract:
+        return ActorRef(self._messenger, local_issuer_id, address)
 
     def issue_node_ref(self, local_issuer_id: str, node_id: str) -> NodeRef:
-        pass
+        return NodeRef(self._messenger, local_issuer_id, node_id)
 
     def register_executable_actor(self, actor: ActorAbstract, actor_id: str):
         self._executor.register(actor_id, actor)
