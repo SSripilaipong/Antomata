3e8675fcdb96535b3fad17927256be0f3d85d319
SSripilaipong <SHSnail@mail.com>
2022-05-04 18:51:09 +0700

    refactor: use ProcessSafeQueueManager() in ProcessSafeInbox instead of raw Manager()
    Refs: #32

--- redcomet/messenger/factory.py
+++ redcomet/messenger/factory.py
@@ -1,11 +1,10 @@
-from multiprocessing import Manager
-
 from redcomet.base.messaging.handler import PacketHandlerAbstract
 from redcomet.messenger import Messenger
 from redcomet.messenger.address_cache import AddressCache
 from redcomet.messenger.inbox.process_safe import ProcessSafeInbox
 from redcomet.messenger.inbox.synchronous import SynchronousInbox
 from redcomet.messenger.outbox import Outbox
+from redcomet.queue.process_safe import ProcessSafeQueueManager
 
 
 def create_messenger(handler: PacketHandlerAbstract, address_cache: AddressCache = None, *,
@@ -13,7 +12,7 @@ def create_messenger(handler: PacketHandlerAbstract, address_cache: AddressCache
     if not parallel:
         inbox = SynchronousInbox(handler)
     else:
-        manager = Manager()
-        inbox = ProcessSafeInbox(manager, manager.Queue(), handler)
+        manager = ProcessSafeQueueManager()
+        inbox = ProcessSafeInbox(manager, manager.start(), handler)
 
     return Messenger(actor_id, inbox, Outbox(), address_cache=address_cache)
