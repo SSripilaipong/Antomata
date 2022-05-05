7fa9205e8b048256471028e80c67cc6b7b997d56
SSripilaipong <SHSnail@mail.com>
2022-05-05 13:12:02 +0700

    refactor: extract factory function from messenger test
    Refs: #32

---
+++ tests/test_messenger/factory.py
@@ -0,0 +1,16 @@
+from redcomet.base.messaging.handler import PacketHandlerAbstract
+from redcomet.messenger.factory import create_messenger
+from redcomet.messenger.inbox.queue import QueueManagerAbstract, QueueAbstract
+from tests.test_messenger.mock import MockPacketHandler, MockQueueManager, MockQueue
+
+
+def create_messenger_with_node_id(node_id: str, handler: PacketHandlerAbstract = None,
+                                  inbox_queue_manager: QueueManagerAbstract = None,
+                                  inbox_queue: QueueAbstract = None):
+    handler = handler or MockPacketHandler()
+    inbox_queue_manager = inbox_queue_manager or MockQueueManager()
+    inbox_queue = inbox_queue or MockQueue()
+    messenger = create_messenger(handler, inbox_queue_manager=inbox_queue_manager, inbox_queue=inbox_queue,
+                                 parallel=True)
+    messenger.assign_node_id(node_id)
+    return messenger
--- tests/test_messenger/test_send_packet.py
+++ tests/test_messenger/test_send_packet.py
@@ -1,28 +1,14 @@
 from redcomet.base.messaging.address import Address
-from redcomet.base.messaging.handler import PacketHandlerAbstract
 from redcomet.base.messaging.packet import Packet
-from redcomet.messenger.factory import create_messenger
 from redcomet.messenger.inbox.message import StopReceiveLoop
-from redcomet.messenger.inbox.queue import QueueAbstract, QueueManagerAbstract
-from tests.test_messenger.mock import MockQueue, MockQueueManager, MockPacketHandler, DummyPacketContent
-
-
-def _create_messenger_with_node_id(node_id: str, handler: PacketHandlerAbstract = None,
-                                   inbox_queue_manager: QueueManagerAbstract = None,
-                                   inbox_queue: QueueAbstract = None):
-    handler = handler or MockPacketHandler()
-    inbox_queue_manager = inbox_queue_manager or MockQueueManager()
-    inbox_queue = inbox_queue or MockQueue()
-    messenger = create_messenger(handler, inbox_queue_manager=inbox_queue_manager, inbox_queue=inbox_queue,
-                                 parallel=True)
-    messenger.assign_node_id(node_id)
-    return messenger
+from tests.test_messenger.factory import create_messenger_with_node_id
+from tests.test_messenger.mock import MockQueue, MockPacketHandler, DummyPacketContent
 
 
 def test_should_send_packet_to_receiver_inbox_queue():
     receiver_inbox_queue = MockQueue()
-    sender = _create_messenger_with_node_id("sender-node")
-    receiver = _create_messenger_with_node_id("receiver-node", inbox_queue=receiver_inbox_queue)
+    sender = create_messenger_with_node_id("sender-node")
+    receiver = create_messenger_with_node_id("receiver-node", inbox_queue=receiver_inbox_queue)
     sender.make_connection_to(receiver)
 
     packet = Packet(DummyPacketContent(), Address.on_local("me"), Address("receiver-node", "you"))
@@ -37,7 +23,7 @@ def test_should_receive_packet_until_StopReceiveLoop():
     receiver_inbox_queue.put(Packet(DummyPacketContent(123), Address("sender", "me"), Address("receiver", "you")))
     receiver_inbox_queue.put(Packet(StopReceiveLoop(), ..., ...))
 
-    receiver = _create_messenger_with_node_id("receiver", inbox_queue=receiver_inbox_queue, handler=receiver_handler)
+    receiver = create_messenger_with_node_id("receiver", inbox_queue=receiver_inbox_queue, handler=receiver_handler)
 
     receiver.start_receive_loop()
 
@@ -47,14 +33,14 @@ def test_should_receive_packet_until_StopReceiveLoop():
 
 def test_should_stop_receive_loop_by_sending_StopReceiveLoop():
     receiver_inbox_queue = MockQueue()
-    receiver = _create_messenger_with_node_id("", inbox_queue=receiver_inbox_queue)
+    receiver = create_messenger_with_node_id("", inbox_queue=receiver_inbox_queue)
     receiver.stop_receive_loop()
     assert receiver_inbox_queue.get() == Packet(StopReceiveLoop(), ..., ...)
 
 
 def test_should_send_packet_to_its_own_inbox_queue():
     inbox_queue = MockQueue()
-    messenger = _create_messenger_with_node_id("node", inbox_queue=inbox_queue)
+    messenger = create_messenger_with_node_id("node", inbox_queue=inbox_queue)
 
     messenger.send_packet(Packet(DummyPacketContent(123), Address.on_local("me"), Address.on_local("me-again")))
     assert inbox_queue.get() == Packet(DummyPacketContent(123), Address("node", "me"), Address("node", "me-again"))
